ba74c25bdca7969077b3df0b8cbc05c9f30c0ccb
ta2019git <take.kendo.0219@ymobile.ne.jp>
2021-12-24 02:21:55 +0900

    I created Main Class for the first time for Github

---
+++ warota-git/src/com/my/git/practice/Main.java
@@ -0,0 +1,9 @@
+package com.my.git.practice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ]
+		System.out.println("This is my first application for Github");
+	}
+}
