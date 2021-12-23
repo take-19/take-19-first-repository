632467cc01d82ec509b44eea9197e7ebc7c0041f
ta2019git <take.kendo.0219@ymobile.ne.jp>
2021-12-24 08:15:37 +0900

    I just added subtle changes to Driver class.

--- warota-git/src/com/my/git/practice/Main.java
+++ warota-git/src/com/my/git/practice/Main.java
@@ -1,9 +1,11 @@
 package com.my.git.practice;
 
 public class Main {
-
+    /*
+     * @Entry Point
+     */
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ]
 		System.out.println("This is my first application for Github");
+		System.out.println("I like Java language");
 	}
 }
